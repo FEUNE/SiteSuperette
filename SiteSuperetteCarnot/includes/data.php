6a55a86326b28e2dc79d3afb574d202f46b48443
azizfombo <118119107+azizfombo@users.noreply.github.com>
2023-02-16 16:48:44 +0100

    Add files via upload

---
+++ SiteSuperetteCarnot/includes/data.php
@@ -0,0 +1,27 @@
+<?php
+
+$slides = array("s1.jpg", "s2.jpg", "s3.jpg", "s4.jpg", "s5.jpg", "s6.jpg", "s7.jpg", "s8.jpg", "s9.jpg", "s10.jpg");
+
+$products = [];
+
+$products[1] = ["image" => "1.jpg", "title" => "Huile Mayor", "price" => "$20.00 - $90.00"];
+$products[2] = ["image" => "2.jpg", "title" => "Sac à dos Dora", "price" => "$35.00 - $130.00"];
+$products[3] = ["image" => "3.jpg", "title" => "T-shirt été", "price" => "$76.00 - $180.00"];
+$products[4] = ["image" => "4.jpg", "title" => "Chaussons Sia", "price" => "$12.00 - $67.00"];
+$products[5] = ["image" => "5.jpg", "title" => "Born to Fly", "price" => "$54.00 - $110.00"];
+$products[6] = ["image" => "6.jpg", "title" => "Sac Gucci", "price" => "$73.00 - $150.00"];
+$products[7] = ["image" => "7.jpg", "title" => "Sac versace", "price" => "$28.00 - $124.00"];
+$products[8] = ["image" => "8.jpg", "title" => "Apple watch (Monchon)", "price" => "$27.00 - $90.00"];
+$products[9] = ["image" => "9.jpg", "title" => "Tricot de Menage", "price" => "$42.00 - $65.00"];
+$products[10] = ["image" => "10.jpg", "title" => "Air Jordan Series 4", "price" => "$47.00 - $84.00"];
+$products[11] = ["image" => "11.jpg", "title" => "Intel POP 54", "price" => "$33.00 - $90.00"];
+$products[12] = ["image" => "12.jpg", "title" => "Samsung Galaxy Z Flip", "price" => "$65.00 - $120.00"];
+$products[13] = ["image" => "13.jpg", "title" => "Coca Cola", "price" => "$15.00 - $30.00"];
+$products[14] = ["image" => "14.jpg", "title" => "Cereales Chocapic", "price" => "$15.00 - $20.00"];
+$products[15] = ["image" => "15.jpg", "title" => "Pates Farfalle", "price" => "$5.00 - $12.50"];
+$products[16] = ["image" => "16.jpg", "title" => "Ordinateur Asus", "price" => "$365.00 - $420.00"];
+$products[17] = ["image" => "17.jpg", "title" => "Playstation 4", "price" => "$365.00 - $416.99"];
+$products[18] = ["image" => "18.jpg", "title" => "Panier de Fruits", "price" => "$20.00 - $30.00"];
+$products[19] = ["image" => "19.jpg", "title" => "Lait Ecreme", "price" => "$5.00 - $20.99"];
+$products[20] = ["image" => "20.jpg", "title" => "Micro Onde DAEVO", "price" => "$423.57 - $650.99"];
+$products[21] = ["image" => "21.jpg", "title" => "Casseroles", "price" => "$35.00 - $62.00"];
